1c1fba7454fbeefa441695a1ce674dda6956f8f0
Shinya Inomata <shinya.inomata@gmail.com>
2020-09-23 16:26:29 +0900

    Initial Commit

---
+++ TodoProductHW20200923/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoProductHW20200923
+//
+//  Created by Shinya  on 2020/09/23.
+//  Copyright © 2020 Shinya . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TodoProductHW20200923Tests/TodoProductHW20200923Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TodoProductHW20200923Tests.swift
+//  TodoProductHW20200923Tests
+//
+//  Created by Shinya  on 2020/09/23.
+//  Copyright © 2020 Shinya . All rights reserved.
+//
+
+import XCTest
+@testable import TodoProductHW20200923
+
+class TodoProductHW20200923Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
