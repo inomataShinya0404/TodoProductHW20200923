65bac43a30d9c6a7624c6ef872b6408f5961ebb1
Shinya Inomata <shinya.inomata@gmail.com>
2020-09-23 16:39:34 +0900

    First commit

---
+++ TodoProductHW20200923/RegisterViewController.swift
@@ -0,0 +1,40 @@
+//
+//  RegisterViewController.swift
+//  TodoProductHW20200923
+//
+//  Created by Shinya  on 2020/09/23.
+//  Copyright © 2020 Shinya . All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func registerButton() {
+    
+    }
+    
+    @IBAction func cancelButton() {
+        
+    }
+
+}
